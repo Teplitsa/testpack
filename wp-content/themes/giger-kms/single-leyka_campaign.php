05f987700e6cde63234e6de8053c05f3f52bc8a6
Anna Ladoshkina <webdev@foralien.com>
2016-06-22 00:49:18 +0300

    Builder as default template for campaign

--- wp-content/themes/giger-kms/single-leyka_campaign.php
+++ wp-content/themes/giger-kms/single-leyka_campaign.php
@@ -71,31 +71,12 @@ if(has_term('programms', 'campaign_cat', $cpost)){
 
 <?php } else { ?>
 
-<article class="main-content leyka-campaign">
-<div class="container">
-	<header class="entry-header-full">		
-		<h1 class="entry-title"><?php echo get_the_title($cpost);?></h1>						
-	</header>
-	
-	<div class="frame">
-		<main class="bit md-8 lg-6">
-		<div class="campaign-form">
-				<?php //echo apply_filters('the_content', ''); ?>
-				<?php rdc_donation_form(); ?>
-			</div>
-		</main>
-	
-		<div id="rdc_sidebar" class="bit md-4 lg-offset-2">
-			<div class="widget donation_cta red"><?php echo apply_filters('rdc_entry_the_content', $cpost->post_content); ?></div>
-			<div class="widget donation_history">
-				<h3><?php _e('Our supporters', 'rdc');?></h3>
-				<?php echo leyka_get_donors_list($cpost->ID, array('num' => 10, 'show_purpose' => 0));?>
-				
-				<div class="all-link"><a href="<?php echo get_permalink($cpost);?>donations"><?php _e('Full list', 'rdc');?>&nbsp;&rarr;</a></div>
-			</div>
-		</div>
+<article id="single-page" class="main-content tpl-page-fullwidth project-page">
+	<div id="rdc_sharing" class="regular-sharing hide-upto-medium"><?php echo rdc_social_share_no_js();?></div>
+		
+	<div class="container">
+		<div class="entry-content"><?php echo apply_filters('the_content', $cpost->post_content); ?></div>
 	</div>
-</div>
 </article>
 <?php
 } //has terms
