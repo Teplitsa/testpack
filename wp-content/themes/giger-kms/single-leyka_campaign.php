c0252af2e1d5329c58e7c6e99fd34957fd824101
Anna Ladoshkina <webdev@foralien.com>
2016-07-19 11:22:03 +0300

    Fix for title at campaign page

--- wp-content/themes/giger-kms/single-leyka_campaign.php
+++ wp-content/themes/giger-kms/single-leyka_campaign.php
@@ -10,12 +10,7 @@ get_header(); ?>
 <section class="main-content single-post-section campaign">
 
 <div class="container">
-	<header class="entry-header-full">		
-		<h1 class="entry-title section-like"><?php echo get_the_title($cpost);?></h1>				
-			
-		<div class="lead"><?php echo apply_filters('tst_the_content', $cpost->post_excerpt); ?></div>
-	</header>
-	
+		
 	<!-- for constuctor -->
 	<div class="entry-content"><?php echo apply_filters('the_content', $cpost->post_content); ?></div>
 </div>
