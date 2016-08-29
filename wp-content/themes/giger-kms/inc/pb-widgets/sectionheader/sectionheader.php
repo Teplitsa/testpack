3e8664e347ed9e3208bf5617fd53b80a8d71ba3b
Anna Ladoshkina <webdev@foralien.com>
2016-08-29 23:43:56 +0300

    Ballon icon

--- wp-content/themes/giger-kms/inc/pb-widgets/sectionheader/sectionheader.php
+++ wp-content/themes/giger-kms/inc/pb-widgets/sectionheader/sectionheader.php
@@ -120,7 +120,7 @@ class IST_PB_SectionHeader_Widget extends SiteOrigin_Widget {
 		$class = (isset($instance['panels_info']['style']['class'])) ? $instance['panels_info']['style']['class'] : '';
 		if(false !== strpos($class, 'bottom-decor')) {
 			echo "<div class='bottom-decor-el'>";
-			tst_svg_icon('icon-balloon');
+			tst_svg_icon('pic-balloon');
 			echo "</div>";
 		}
 		echo '</div>';
