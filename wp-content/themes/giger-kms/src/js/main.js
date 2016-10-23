35e5088a8c978945b60320aea513b39ca1cf473e
sergio_ryabinin <sgresr.test@gmail.com>
2016-10-23 23:09:57 +0200

    adding animation to blocks

--- wp-content/themes/giger-kms/src/js/main.js
+++ wp-content/themes/giger-kms/src/js/main.js
@@ -1,67 +1,72 @@
 /* Scripts */
-(function($){
-    $.fn.viewportChecker = function(useroptions){
-        // Define options and extend with user
-        var options = {
-            classToAdd: 'visible',
-            offset: 100,
-            callbackFunction: function(elem){}
-        };
-        $.extend(options, useroptions);
-
-        // Cache the given element and height of the browser
-        var $elem = this,
-            windowHeight = $(window).height();
-
-        this.checkElements = function(){
-            // Set some vars to check with
-            var scrollElem = ((navigator.userAgent.toLowerCase().indexOf('webkit') != -1) ? 'body' : 'html'),
-                viewportTop = $(scrollElem).scrollTop(),
-                viewportBottom = (viewportTop + windowHeight);
-
-            $elem.each(function(){
-                var $obj = $(this);
-                // If class already exists; quit
-                if ($obj.hasClass(options.classToAdd)){
-                    return;
-                }
-
-                // define the top position of the element and include the offset which makes is appear earlier or later
-                var elemTop = Math.round( $obj.offset().top ) + options.offset,
-                    elemBottom = elemTop + ($obj.height());
-
-                // Add class if in viewport
-                if ((elemTop < viewportBottom) && (elemBottom > viewportTop)){
-                    $obj.addClass(options.classToAdd);
-
-                    // Do the callback function. Callback wil send the jQuery object as parameter
-                    options.callbackFunction($obj);
-                }
-            });
-        };
-
-        // Run checkelements on load and scroll
-        $(window).scroll(this.checkElements);
-        this.checkElements();
-
-        // On resize change the height var
-        $(window).resize(function(e){
-            windowHeight = e.currentTarget.innerHeight;
-        });
-    };
-})(jQuery);
+// (function($){
+//     $.fn.viewportChecker = function(useroptions){
+//         // Define options and extend with user
+//         var options = {
+//             classToAdd: 'visible',
+//             offset: 100,
+//             callbackFunction: function(elem){}
+//         };
+//         $.extend(options, useroptions);
+// 
+//         // Cache the given element and height of the browser
+//         var $elem = this,
+//             windowHeight = $(window).height();
+// 
+//         this.checkElements = function(){
+//             // Set some vars to check with
+//             var scrollElem = ((navigator.userAgent.toLowerCase().indexOf('webkit') != -1) ? 'body' : 'html'),
+//                 viewportTop = $(scrollElem).scrollTop(),
+//                 viewportBottom = (viewportTop + windowHeight);
+// 
+//             $elem.each(function(){
+//                 var $obj = $(this);
+//                 // If class already exists; quit
+//                 if ($obj.hasClass(options.classToAdd)){
+//                     return;
+//                 }
+// 
+//                 // define the top position of the element and include the offset which makes is appear earlier or later
+//                 var elemTop = Math.round( $obj.offset().top ) + options.offset,
+//                     elemBottom = elemTop + ($obj.height());
+// 
+//                 // Add class if in viewport
+//                 if ((elemTop < viewportBottom) && (elemBottom > viewportTop)){
+//                     $obj.addClass(options.classToAdd);
+// 
+//                     // Do the callback function. Callback wil send the jQuery object as parameter
+//                     options.callbackFunction($obj);
+//                 }
+//             });
+//         };
+// 
+//         // Run checkelements on load and scroll
+//         $(window).scroll(this.checkElements);
+//         this.checkElements();
+// 
+//         // On resize change the height var
+//         $(window).resize(function(e){
+//             windowHeight = e.currentTarget.innerHeight;
+//         });
+//     };
+// })(jQuery);
 
 jQuery(document).ready(function($){
-		$('.city-statistics, .state-statistics').hide();
-    $('.city-statistics').show().removeClass('hidden').viewportChecker({
-      classToAdd: 'visible animated fadeInLeft'
-      //offset: 100
-     });
-    $('.state-statistics').show().removeClass('hidden').viewportChecker({
-      classToAdd: 'visible animated fadeInRight'
-      //offset: 100
-     });
-	
+		// $('.city-statistics, .state-statistics').hide();
+    // $('.city-statistics').show().removeClass('hidden').viewportChecker({
+    //   classToAdd: 'visible animated fadeInLeft'
+    //   //offset: 100
+    //  });
+    // $('.state-statistics').show().removeClass('hidden').viewportChecker({
+    //   classToAdd: 'visible animated fadeInRight'
+    //   //offset: 100
+    //  });
+    $('.so-widget-tst-featureblock .col3').attr('data-aos', 'fade-up');
+    // $('.state-statistics').attr('data-aos', 'fade-left');
+    AOS.init({
+      duration: 600
+    });
+    // 
 	/** Has js **/
 	$('html').removeClass('no-js').addClass('js');
 	
