ea1b78fa5b4c5608be1cf58441404181c6786ed7
Anna Ladoshkina <webdev@foralien.com>
2017-01-24 02:13:50 +0300

    FAvicon

--- wp-content/themes/giger-kms/searchform.php
+++ wp-content/themes/giger-kms/searchform.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * The template for displaying search forms 
+ * The template for displaying search forms
  */
 ?>
 <form method="get" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
-	<input type="search" class="searchform__field" name="s" value="<?php echo esc_attr( get_search_query() ); ?>" autocomplete="off">		
-	<button class="searchform__icon" type="submit"><?php tst_svg_icon('icon-search');?></button>	
+	<input type="search" class="searchform__field" name="s" value="<?php echo esc_attr( get_search_query() ); ?>" autocomplete="off" placeholder="Найти">
+	<button class="searchform__icon" type="submit"><?php tst_svg_icon('icon-search');?></button>
 </form>
