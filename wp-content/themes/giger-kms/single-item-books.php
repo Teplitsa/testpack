c8e8637e82e431dc7607243e47bdb4b3004965fc
DenisCherniatev <denis.cherniatev@gmail.com>
2017-01-25 04:41:26 +0200

    revision commit

--- wp-content/themes/giger-kms/single-item-books.php
+++ wp-content/themes/giger-kms/single-item-books.php
@@ -27,7 +27,8 @@ get_header();
     		
 	?>
     	
-		<div class="bit md-8 lg-8 single-body">
+		<div class="bit md-8 lg-8">
+			<div class="layout-section layout-section--card">
 			<?php
 			    foreach($books as $i => $cpost) {
 			?>
@@ -35,7 +36,8 @@ get_header();
 			<?php
 				}
 			?>
-			<div class="sharing"><?php tst_social_share($item->post_object);?></div>
+				<div class="sharing"><?php tst_social_share($item->post_object);?></div>
+			</div>
 		</div>
 		<div class="bit md-3 lg-3 lg-offset-1 single-side">
 			<?php if( $item->post_name == 'your-question' ):?>
