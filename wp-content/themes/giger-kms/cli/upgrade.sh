99c2ae1c5d33fed8660e54ae6518aa0bd380e9e5
ahaenor <ahaenor@gmail.com>
2017-02-14 15:20:11 +0300

    * Fix for events test content import script.

--- wp-content/themes/giger-kms/cli/upgrade.sh
+++ wp-content/themes/giger-kms/cli/upgrade.sh
@@ -12,6 +12,7 @@ php cli-u-import-posts-csv.php --host=$HOST --file=dront_urls_news_content.csv
 php cli-u-import-files-csv.php --host=$HOST --file=dront_urls_bereginya.csv --tag=bereginya
 php cli-u-import-files-csv.php --host=$HOST --file=dront_urls_publications.csv --tag=publication
 php cli-u-import-files-csv.php --host=$HOST --file=dront_urls_reports.csv --tag=report
+php cli-import-events-csv.php --host=$HOST --file=dront_events_all.csv
 
 #new structure
 #php cli-u-sections_init.php --host=$HOST
