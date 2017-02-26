f624836016c02ed495a9466744632c1664310afa
sergio_riabinin <sgresr@gmail.com>
2017-02-26 11:51:47 +0200

    decrease padding and margins

--- wp-content/themes/giger-kms/admin/general.php
+++ wp-content/themes/giger-kms/admin/general.php
@@ -326,3 +326,8 @@ function tst_thumbnail_dimensions_check($thumbnail_html, $post_id) {
 
     return $thumbnail_html;
 }
+add_action( 'admin_enqueue_scripts', 'load_admin_style' );
+function load_admin_style() {
+	wp_enqueue_style( 'admin_css', get_template_directory_uri() . '/assets/css/admin.css', false, '1.0.0' );
+}
+
