4f1620ba942d257a4fffb56dada65fbc114a09e0
DenisCherniatev <denis.cherniatev@gmail.com>
2017-02-28 00:43:25 +0200

    create tags by each landing news and archive pages tagged upgrade script changed

--- wp-content/themes/giger-kms/cli/upgrade.sh
+++ wp-content/themes/giger-kms/cli/upgrade.sh
@@ -36,6 +36,9 @@ php cli-u-projects.php --host=$HOST
 php cli-u-import-landings-csv.php --host=$HOST --file=data/landings.csv
 php cli-u-import-projects-csv.php --host=$HOST --file=data/projects.csv
 
+# setup tags
+php cli-u-tags.php --host=$HOST
+
 # set posts thumbnails
 php cli-u-posts-thumbnails.php --host=$HOST
 
