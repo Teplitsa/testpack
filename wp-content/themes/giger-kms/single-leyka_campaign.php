61197a71e686d65216a2d828fd2bd2b8ca7376b6
Ahaenor <ahaenor@gmail.com>
2017-03-20 21:56:53 +0300

    * Donations list bugfix - temp. fixed via removing the link from campaigns page.

--- wp-content/themes/giger-kms/single-leyka_campaign.php
+++ wp-content/themes/giger-kms/single-leyka_campaign.php
@@ -13,7 +13,7 @@ get_header(); ?>
 	<div class="campaign-form"><?php tst_donation_form($campaign_post->ID);?></div>
 	<div class="entry-content">
 		<?php echo apply_filters('tst_entry_the_content', $campaign_post->post_content); ?>
-		<div class="all-link"><span class="linked"><a href="<?php echo get_permalink(get_post()->ID);?>donations/">Благодарим за помощь&nbsp;</a>&gt;</span></div>
+<!--		<div class="all-link"><span class="linked"><a href="--><?php //echo get_permalink(get_post()->ID);?><!--donations/">Благодарим за помощь&nbsp;</a>&gt;</span></div>-->
 	</div>
 </div>
 </section>
