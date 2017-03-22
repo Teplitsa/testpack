616a77a949e21bcfdf7fcb34ced43662734b4345
ahaenor <ahaenor@gmail.com>
2017-03-22 15:52:18 +0300

    * New meta fields for project placement - WIP.

--- wp-content/themes/giger-kms/inc/request.php
+++ wp-content/themes/giger-kms/inc/request.php
@@ -15,11 +15,11 @@ function tst_project_archive_ordering(WP_Query $query){
     if($query->is_post_type_archive('project') && $query->is_main_query()) {
 
         $query->set('meta_query', array(
-            'relation' => 'AND',
+//            'relation' => 'AND',
             array(
                 'key'     => 'exclude_from_archive',
                 'value'   => 'true',
-                'compare' => 'LIKE',
+                'compare' => '!=',
             ),
         ));
 
